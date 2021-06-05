c63edcbdd73cb9ae303664c3aa65093e46cbff09
harsh <harshgkanjariya2468@gmail.com>
2021-06-05 16:30:20 +0530

    png error solved

--- src/steganography.class.js
+++ src/steganography.class.js
@@ -51,13 +51,13 @@ class Steganography{
 			buffer = Buffer.from(data);
 			str = buffer.toString('ascii');
 		}else if (type === 'png'){
-			for (let i=buf.length-1;i>=0; i--){
-				if (buf[i] === 0x82 && buf[i-1] === 0x60 && buf[i-2] === 0x42 && buf[i-3] === 0xae &&
-					buf[i-4] === 0x44 && buf[i-5] === 0x4e && buf[i-6] === 0x45 && buf[i-7] === 0x49 &&
-					buf[i-8] === 0x00 && buf[i-9] === 0x00 && buf[i-10] === 0x00 && buf[i-11] === 0x00){
+			for (let i=buffer.length-1;i>=0; i--){
+				if (buffer[i] === 0x82 && buffer[i-1] === 0x60 && buffer[i-2] === 0x42 && buffer[i-3] === 0xae &&
+					buffer[i-4] === 0x44 && buffer[i-5] === 0x4e && buffer[i-6] === 0x45 && buffer[i-7] === 0x49 &&
+					buffer[i-8] === 0x00 && buffer[i-9] === 0x00 && buffer[i-10] === 0x00 && buffer[i-11] === 0x00){
 					break;
 				}
-				data.push(buf[i]);
+				data.push(buffer[i]);
 			}
 			data.reverse();
 			buffer = Buffer.from(data);
